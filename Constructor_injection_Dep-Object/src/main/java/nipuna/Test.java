9d09ca7eeb1ff8738da9443f04b1c83f00c9cbdd
sriramnalla30 <114870983+sriramnalla30@users.noreply.github.com>
2024-07-01 10:24:50 +0530

    Add files via upload

---
+++ Constructor_injection_Dep-Object/src/main/java/nipuna/Test.java
@@ -0,0 +1,17 @@
+package nipuna;
+
+import org.springframework.beans.factory.BeanFactory;  
+import org.springframework.beans.factory.xml.XmlBeanFactory;  
+import org.springframework.core.io.*;  
+  
+public class Test {  
+    public static void main(String[] args) {  
+          
+        Resource r=new ClassPathResource("ac.xml");  
+        BeanFactory factory=new XmlBeanFactory(r);  
+          
+        Employee s=(Employee)factory.getBean("e");  
+        s.show();  
+          
+    }  
+}  
